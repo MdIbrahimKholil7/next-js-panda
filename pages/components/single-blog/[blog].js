f21a6b9971cca5128c3222cdb6527c6c4c42ce97
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-10-30 12:14:46 +0600

    dangerously html add

--- pages/components/single-blog/[blog].js
+++ pages/components/single-blog/[blog].js
@@ -5,25 +5,22 @@ const SingleBlog = (props) => {
     console.log(props)
     const router = useRouter()
     const { blog } = router.query
-    // console.log(blog)
+    function createMarkup(c) {
+        return { __html: c };
+    }
     return (
         <div>
-            <div
-                className='text-center mb-20 cursor-pointer mt-20'
-            >
-                <h4 className='text-3xl font-bold pb-5'>{props?.result?.data?.title}</h4>
-                <p className='text-left px-60'>{props?.result?.data?.content?.substr(0, 250)}</p>
-            </div>
+            <div className='text-center mb-20 cursor-pointer mt-20'
+                dangerouslySetInnerHTML={createMarkup(props?.result?.data?.content)}></div>
         </div>
     );
 };
 
 export async function getServerSideProps(context) {
 
-    // console.log('from blog',context.query.blog)
     const fetchData = await fetch(`http://localhost:3000/api/singleBlog?title=${context.query.blog}`)
     const result = await fetchData.json()
-    console.log(result)
+
     return {
         props: { result }, // will be passed to the page component as props
     }
